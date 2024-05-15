d3fbd98ebba206e87dd71d57f20e55c7acb45b86
Camilo Carmona Valencia <101128930+Camilin1904@users.noreply.github.com>
2024-05-14 20:15:14 -0500

    Fix database oversight

--- src/main/java/co/edu/icesi/sgiv/domain/entity/Plan.java
+++ src/main/java/co/edu/icesi/sgiv/domain/entity/Plan.java
@@ -27,9 +27,6 @@ public class Plan {
     @Column(name = "code", nullable = false, length = 50)
     private String code;
 
-    @Column(name = "name", nullable = false, length = 100)
-    private String name;
-
     @Column(name = "number_of_people", nullable = false)
     private Integer numberOfPeople;
 
